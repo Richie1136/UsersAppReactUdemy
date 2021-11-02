d81ae316ed0991d80c0643f9630fc0eebfcd0292
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-02 11:13:35 -0400

    Created two ReactDOM portals to move the backdrop and modal above the rest of the content

--- usersapp/src/components/modal/ErrorModal.js
+++ usersapp/src/components/modal/ErrorModal.js
@@ -2,23 +2,32 @@ import React from 'react'
 import Card from '../card/Card'
 import Button from '../button/Button'
 import './ErrorModal.css'
+import ReactDOM from 'react-dom'
+
+const Backdrop = ({ onerrorHandler }) => {
+  return <div className='backdrop' onClick={onerrorHandler} />
+}
+
+const ModalOverlay = ({ title, message, onerrorHandler }) => {
+  return <Card classname='modal'>
+    <header className='header'>
+      <h2>{title}</h2>
+    </header>
+    <div className='content'>
+      <p>{message}</p>
+    </div>
+    <footer className='actions'>
+      <Button onClick={onerrorHandler}>OK</Button>
+    </footer>
+  </Card>
+}
 
 const ErrorModal = ({ title, message, onerrorHandler }) => {
   return (
-    <div>
-      <div className='backdrop' onClick={onerrorHandler} />
-      <Card classname='modal'>
-        <header className='header'>
-          <h2>{title}</h2>
-        </header>
-        <div className='content'>
-          <p>{message}</p>
-        </div>
-        <footer className='actions'>
-          <Button onClick={onerrorHandler}>OK</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(<Backdrop onerrorHandler={onerrorHandler} />, document.getElementById('backdrop-root'))}
+      {ReactDOM.createPortal(<ModalOverlay title={title} message={message} onerrorHandler={onerrorHandler} />, document.getElementById('modal-root'))}
+    </>
   )
 }
 
